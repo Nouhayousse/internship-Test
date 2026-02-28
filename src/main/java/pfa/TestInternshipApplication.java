588778d7b7c82ad6ffd3e4d114b18ff6ef754701
Youssefi Nouhaila <nouhaila.youssefi@um5r.ac.ma>
2026-02-28 17:17:06 +0000

    first commit

---
+++ src/main/java/pfa/TestInternshipApplication.java
@@ -0,0 +1,13 @@
+package pfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestInternshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestInternshipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pfa/TestInternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package pfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestInternshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
