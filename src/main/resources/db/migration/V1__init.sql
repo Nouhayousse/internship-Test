18a3d63aaf558e8623381491083bcb9f0eadcdd4
Youssefi Nouhaila <nouhaila.youssefi@um5r.ac.ma>
2026-03-01 03:06:11 +0000

    reglage de flyway pour versionnement des DB

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,7 +2,11 @@ CREATE TABLE users (
                        id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
                        name        VARCHAR(100) NOT NULL,
                        email       VARCHAR(150) NOT NULL UNIQUE,
-                       created_at  TIMESTAMP NOT NULL DEFAULT now()
+    -- colonnes AuditEntity
+                       created_by  VARCHAR(100),
+                       created_on  TIMESTAMP,
+                       updated_by  VARCHAR(100),
+                       updated_on  TIMESTAMP
 );
 
 CREATE TABLE resources (
@@ -11,7 +15,11 @@ CREATE TABLE resources (
                            description TEXT,
                            capacity    INT NOT NULL DEFAULT 1,
                            active      BOOLEAN NOT NULL DEFAULT true,
-                           created_at  TIMESTAMP NOT NULL DEFAULT now()
+    -- colonnes AuditEntity
+                           created_by  VARCHAR(100),
+                           created_on  TIMESTAMP,
+                           updated_by  VARCHAR(100),
+                           updated_on  TIMESTAMP
 );
 
 CREATE TABLE reservations (
@@ -21,12 +29,14 @@ CREATE TABLE reservations (
                               start_time   TIMESTAMP NOT NULL,
                               end_time     TIMESTAMP NOT NULL,
                               status       VARCHAR(20) NOT NULL DEFAULT 'CONFIRMED',
-                              created_at   TIMESTAMP NOT NULL DEFAULT now(),
-
-                              CONSTRAINT chk_times CHECK (end_time > start_time)
+                              CONSTRAINT chk_times CHECK (end_time > start_time),
+    -- colonnes AuditEntity
+                              created_by  VARCHAR(100),
+                              created_on  TIMESTAMP,
+                              updated_by  VARCHAR(100),
+                              updated_on  TIMESTAMP
 );
 
--- Index critique pour les checks de conflits (performance)
 CREATE INDEX idx_reservations_resource_time
     ON reservations(resource_id, start_time, end_time)
     WHERE status != 'CANCELLED';
